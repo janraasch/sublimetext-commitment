bfa630ea8d8ec57971fd70ca999808f7f9aae59c
Jan Raasch <janraasch@gmail.com>
2013-05-08 20:06:21 +0200

    Bunny approves these changes.
    (\ /)
    (O.o)
    (> <)

--- commit.py
+++ commit.py
@@ -1,30 +1,36 @@
-import os
 import random
+import sublime
 
 try:
     from hashlib import md5
 except ImportError:
     from md5 import md5
 
-names = ['Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd',
-    'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali', 'Kim',
-    'Rainer', 'Guillaume']
-
-messages_file = os.path.join(os.path.dirname(__file__), 'commit_messages.txt')
+commitMessages = 'commit_messages.txt'
 messages = {}
 
+st_version = 2
+if sublime.version() == '' or int(sublime.version()) > 3000:
+    st_version = 3
+
 # Create a hash table of all commit messages
-try:
-    # Python 3
-    for line in open(messages_file, encoding='utf-8').readlines():
-        messages[md5(line.encode('utf-8')).hexdigest()] = line
-except (TypeError):
-    # Python 2
-    for line in open(messages_file).readlines():
+if st_version == 3:
+    from zipfile import ZipFile
+    messages_file = ZipFile(sublime.installed_packages_path() + '/Commitment.sublime-package').open(commitMessages)
+    for line in messages_file.readlines():
+        messages[md5(line).hexdigest()] = line
+else:
+    from os import path
+    messages_file = open(path.join(path.dirname(__file__), commitMessages))
+    for line in messages_file.readlines():
         messages[md5(line).hexdigest()] = line
 
 # Create list of hashes
 message_hashes = list(messages.keys())
+
+names = ['Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd',
+    'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali', 'Kim',
+    'Rainer', 'Guillaume']
 
 class Commitment:
     def get(self, message_hash=None):
